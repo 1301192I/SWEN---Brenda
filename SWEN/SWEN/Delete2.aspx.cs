fb0f6ace502058e3d1fb162e5be2de83ee66702f
Brenda <1301192I@users.noreply.github.com>
2015-06-03 10:04:52 +0800

    Update Delete2.aspx.cs

--- SWEN/SWEN/Delete2.aspx.cs
+++ SWEN/SWEN/Delete2.aspx.cs
@@ -9,6 +9,7 @@ namespace SWEN.Room_Booking_Module
 {
     public partial class Delete2 : System.Web.UI.Page
     {
+        //This page is to prompt the users if they are sure they want to cancek (delete) the booking
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,11 +17,13 @@ namespace SWEN.Room_Booking_Module
 
         protected void btnDeleteNo_Click(object sender, EventArgs e)
         {
+            //if no is clicked the user will return to the previous page
             Response.Redirect("Delete1.aspx");
         }
 
         protected void btnDeleteYes_Click(object sender, EventArgs e)
         {
+            //if yes is click, the booking will be cancelled and the user will be told that the booking is successfully cancelled
             Response.Redirect("Delete3.aspx");
         }
     }
