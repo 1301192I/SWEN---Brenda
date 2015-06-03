192ce273f4c655fd7a7d21bc9ae2f7fe58fd0ab6
Brenda <1301192I@users.noreply.github.com>
2015-06-03 09:34:01 +0800

    Update Delete1.aspx.cs

--- SWEN/SWEN/Delete1.aspx.cs
+++ SWEN/SWEN/Delete1.aspx.cs
@@ -8,6 +8,7 @@ using SWEN.Classes;
 
 namespace SWEN.Room_Booking_Module
 {
+    //Brenda delete page where users are allowed to cancel their bookings
     public partial class DeleteRoom : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
@@ -17,11 +18,14 @@ namespace SWEN.Room_Booking_Module
 
         protected void btnDeleteHomepage_Click(object sender, EventArgs e)
         {
+            //this button is to direct the users back to the home page
             Response.Redirect("homeAM.aspx");
         }
 
         protected void btnDeleteSearch_Click(object sender, EventArgs e)
         {
+            //This is to delete from the following tables in the database. 
+            //There are 3 different tables affected because they are linked together by the foriegn keys
             SWENDBManager.DeleteBedType(Convert.ToInt32(tbxDelete.Text));
             SWENDBManager.DeleteRoomType(Convert.ToInt32(tbxDelete.Text));
             SWENDBManager.DeleteBooking(Convert.ToInt32(tbxDelete.Text));
